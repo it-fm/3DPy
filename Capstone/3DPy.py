74008696cf69fae18f60f146ccc5ad10e41e66f6
lmpynix <lmpynix@gmail.com>
2016-12-11 12:50:27 -0600

    Added licensing information.

--- Capstone/3DPy.py
+++ Capstone/3DPy.py
@@ -1,14 +1,32 @@
 # Capstone project, 3DPy Cost Calculator
 # Function: A program that allows a user to easily calculate the cost of a 3-D Printed object, with the ability to save and retrieve material data to and from file.
 
+#Copyright (c) 2016, Logan Power
+#All rights reserved.
+#
+#Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#
+#1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#
+#2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+#THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+#IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+#(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
 # Import my modules.  WORKS.
 from myUi import *
 from myFileFxs import *
 from myFunctions import *
 from myVars import *
 
-INTROTEXT = """3DPy Cost Calculator -- Version 1.0.5
-    Copyright (c) 2016 Logan Power.
+INTROTEXT = """3DPy Cost Calculator -- Version 1.0.7
+    Copyright (c) 2016, Logan Power
+    All Rights Reserved.
+    This software is released under the terms of the FreeBSD License, included with this software in LICENSE.txt
 """
 
 exitVal = False
